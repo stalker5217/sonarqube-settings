c89be8c27a6796abe456dec0b1d84fde96165590
Song <stalker5217@gmail.com>
2022-01-24 19:24:40 +0900

    first commit

---
+++ src/main/java/com/example/sonarqube/SonarqubeSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeSettingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubeSettingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sonarqube/SonarqubeSettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeSettingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
